556100acdbef8243f0dbaf09db339fdf7f9b0a7f
James28Dev <James28.Dev@gmail.com>
2026-03-01 23:16:49 +0700

    Refactor main.dart by updating the AppBar title and moving the Home widget to a separate file.

--- person_registry_app/lib/main.dart
+++ person_registry_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:person_registry_app/screens/home.dart';
 
 void main() {
   runApp(
@@ -6,7 +7,7 @@ void main() {
       title: "My title",
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("My App"),
+          title: const Text("My Application"),
           backgroundColor: Colors.blue,
           centerTitle: true,
         ),
@@ -14,21 +15,4 @@ void main() {
       ),
     ),
   );
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.orange,
-      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-      padding: const EdgeInsets.all(20),
-      child: const Text(
-        "JamesDev",
-        style: TextStyle(fontSize: 30, letterSpacing: 3),
-      ),
-    );
-  }
 }
