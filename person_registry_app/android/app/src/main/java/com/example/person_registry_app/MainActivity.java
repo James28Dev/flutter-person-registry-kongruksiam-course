3513ca2337decb4f4e420f23d6164030dfc075e3
James28Dev <James28.Dev@gmail.com>
2026-03-01 18:27:43 +0700

    Update project configuration files to include Dart SDK and Dart Packages libraries in the IntelliJ IDEA module.

---
+++ person_registry_app/android/app/src/main/java/com/example/person_registry_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.person_registry_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- person_registry_app/android/app/src/main/kotlin/com/example/person_registry_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.person_registry_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
