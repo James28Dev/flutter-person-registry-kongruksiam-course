8af7b17837c833978220870e9144ef59b8dd7070
James28Dev <James28.Dev@gmail.com>
2026-03-01 23:15:27 +0700

    Add the `Home` widget in `person_registry_app/lib/screens/home.dart` featuring a styled text container.

---
+++ person_registry_app/lib/screens/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.orange,
+        padding: const EdgeInsets.all(5),
+        child: const Text(
+          "JamesDev",
+          style: TextStyle(fontSize: 30, letterSpacing: 3),
+        ),
+      ),
+    );
+  }
+}
