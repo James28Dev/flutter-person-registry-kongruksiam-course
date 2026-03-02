356d4d39cd196c77b62d3ec4ae90e86730ba11c1
James28Dev <James28.Dev@gmail.com>
2026-03-02 22:56:30 +0700

    Refactor `main.dart` by extracting the `MaterialApp` into a new `MyApp` stateless widget.

--- person_registry_app/lib/main.dart
+++ person_registry_app/lib/main.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:person_registry_app/screens/home.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: "My title",
       home: Scaffold(
         appBar: AppBar(
@@ -13,6 +20,6 @@ void main() {
         ),
         body: const Home(),
       ),
-    ),
-  );
+    );
+  }
 }
